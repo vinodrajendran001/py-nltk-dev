1ca21540e196bcc41801eb35f8db2a7a9fc1ba29
ivonyte@gmail.com <ivonyte@gmail.com@f85f9711-d061-18d5-81b2-01f3bb4b83c6>
2013-05-07 21:45:24 +0000

    Male != Female.

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ TRAINING_SET_SIZE = 20 				  # percentage of training set size
 MAX_TRAINING_ITERS = 5				  # number of MaxEntClassifier iteration before stopping
 CLASSIFIER_FILE = 'classifier.pickle' # dump pickled classifier to this file
 MALE_NAME_FILE = "names/male.txt"
-FEMALE_NAME_FILE = "names/male.txt"
+FEMALE_NAME_FILE = "names/female.txt"
 CITY_FILE = "cities/world_cities.txt"
