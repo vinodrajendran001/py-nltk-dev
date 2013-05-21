49141dfd025cb80a01f2fc2818ded873c889a059
ivonyte@gmail.com <ivonyte@gmail.com@f85f9711-d061-18d5-81b2-01f3bb4b83c6>
2013-05-21 22:19:28 +0000

    Still working on output.

--- trunk/main.py
+++ trunk/main.py
@@ -1,57 +1,68 @@
 import nltk, data, summarize, pickle, ner, config, training, sys, utils, action, references
-
 from nltk.tree import Tree
-
-MAX_SENTENCES = config.MAX_SENTENCES
-
-# load article text
-path = "db/ivonyte-aiste/2011-7-8-1.txt" 
-article = data.Article(path)
-utils.load_data(article.text)
-
-fp = file("results.txt", "wb")
-
 
 def print_to_screen_and_file(text):
 	print text
 	if type(text) == list:
-		for item in text:
-			fp.write("%s\n" % item)
+		for index, item in enumerate(text):
+			print index, item
 	else:
 		fp.write(text)
 		fp.write("\n")
 
-# show article text
-print "-"*80
-print "Original article:"
-print article.text
-print "-"*80
+def run(path):
+	global fp 
+	MAX_SENTENCES = config.MAX_SENTENCES	
 
-# make the summary & show in console
-print_to_screen_and_file("I Summary:")
-instance = summarize.SimpleSummarizer()
-print_to_screen_and_file(instance.summarize(article.text, MAX_SENTENCES))
-print_to_screen_and_file("-"*80)
+	if len(path) == 0:
+		path = "db/ivonyte-aiste/2011-7-8-1.txt" 
+	# load article text
 
-print_to_screen_and_file("II Summary:")
-print_to_screen_and_file("Will be done soon :) ")
-print_to_screen_and_file("-"*80)
+	article = data.Article(path)
+	utils.load_data(article.text)
+	
+	fp = file("results.txt", "wb")
 
-print_to_screen_and_file("People and their actions:")
-work = action.Actions().find(utils.tagged_words, utils.tagged_sentences, utils.people)
-# print the updated info with people actions
-for i, (key, value) in enumerate(work.items()):
-	print_to_screen_and_file("[%d] - %s = %s"%(i+1, key, value))
-print_to_screen_and_file("-"*80)
+	# show article text
+	print_to_screen_and_file("-"*80)
+	print_to_screen_and_file("Original article:")
+	print_to_screen_and_file(article.text)
+	print_to_screen_and_file("-"*80)
 
-print_to_screen_and_file("Anaphoras:")
-refs = references.References().find(utils.people, utils.sentences, utils.tagged_sentences)
-print_to_screen_and_file(refs)
-print_to_screen_and_file("-"*80)
+	# make the summary & show in console
+	print_to_screen_and_file("I Summary:")
+	instance = summarize.SimpleSummarizer()
+	print_to_screen_and_file(instance.summarize(article.text, MAX_SENTENCES))
+	print_to_screen_and_file("-"*80)
 
+	print_to_screen_and_file("II Summary:")
+	print_to_screen_and_file("Will be done soon :) ")
+	print_to_screen_and_file("-"*80)
 
+	print_to_screen_and_file("People and their actions:")
+	work = action.Actions().find(utils.tagged_words, utils.tagged_sentences, utils.people)
+	# print the updated info with people actions
+	for i, (key, value) in enumerate(work.items()):
+		print_to_screen_and_file("[%d] - %s = %s"%(i+1, key, value))
+	print_to_screen_and_file("-"*80)
 
-print_to_screen_and_file("People interactions:")
-print_to_screen_and_file("-"*80)
+	print_to_screen_and_file("Anaphoras:")
+	refs = references.References().find(utils.people, utils.sentences, utils.tagged_sentences)
+	print_to_screen_and_file(refs)
+	print_to_screen_and_file("-"*80)
 
-fp.close()
+	print_to_screen_and_file("People interactions:")
+	print_to_screen_and_file("-"*80)
+
+	fp.close()
+
+# script start spot
+if __name__ == "__main__":
+	#path = sys.argv[1]
+	#print path
+
+	path = "db/ivonyte-aiste/2011-7-8-1.txt" 
+	run(path)
+
+	#else:
+	
