838c21491798a70ad57d45002e72d049546b7383
ivonyte@gmail.com <ivonyte@gmail.com@f85f9711-d061-18d5-81b2-01f3bb4b83c6>
2013-05-21 21:39:14 +0000

    Making output look pretty.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import nltk, data, summarize, pickle, ner, config, training, sys, utils, action
+import nltk, data, summarize, pickle, ner, config, training, sys, utils, action, references
 
 from nltk.tree import Tree
 
@@ -9,42 +9,49 @@ path = "db/ivonyte-aiste/2011-7-8-1.txt"
 article = data.Article(path)
 utils.load_data(article.text)
 
+fp = file("results.txt", "wb")
+
+
+def print_to_screen_and_file(text):
+	print text
+	if type(text) == list:
+		for item in text:
+			fp.write("%s\n" % item)
+	else:
+		fp.write(text)
+		fp.write("\n")
+
 # show article text
 print "-"*80
+print "Original article:"
 print article.text
 print "-"*80
 
 # make the summary & show in console
-print "I Summary:"
+print_to_screen_and_file("I Summary:")
 instance = summarize.SimpleSummarizer()
-print instance.summarize(article.text, MAX_SENTENCES)
-print "-"*80
+print_to_screen_and_file(instance.summarize(article.text, MAX_SENTENCES))
+print_to_screen_and_file("-"*80)
 
-print "II Summary:"
-print "Will be done soon :) "
-print "-"*80
+print_to_screen_and_file("II Summary:")
+print_to_screen_and_file("Will be done soon :) ")
+print_to_screen_and_file("-"*80)
 
-#print "Tokenizing article words..."
-# tokenize & tag all words in article
-#tokens = nltk.tokenize.wordpunct_tokenize(article.text)
-#tagged_words = nltk.pos_tag(tokens)
-
-# extract & tokenize each sentence separately
-#print "Tokenizing sentences..."
-#sentences = nltk.tokenize.sent_tokenize(article.text)
-#tokenized_sentences = [nltk.tokenize.wordpunct_tokenize(s) for s in sentences] 
-#tagged_sentences = [nltk.pos_tag(s) for s in tokenized_sentences]
-
-print "People and their actions:"
-#instance  = ner.NERFinder()
-#people = instance.find(utils.tagged_words, utils.sentences, utils.tagged_sentences)
+print_to_screen_and_file("People and their actions:")
 work = action.Actions().find(utils.tagged_words, utils.tagged_sentences, utils.people)
 # print the updated info with people actions
 for i, (key, value) in enumerate(work.items()):
-	print "[%d] - %s = %s"%(i+1, key, value)
-print "-"*80
+	print_to_screen_and_file("[%d] - %s = %s"%(i+1, key, value))
+print_to_screen_and_file("-"*80)
 
-print "People interactions:"
-print "Will be done soon too ! "
-print "-"*80
+print_to_screen_and_file("Anaphoras:")
+refs = references.References().find(utils.people, utils.sentences, utils.tagged_sentences)
+print_to_screen_and_file(refs)
+print_to_screen_and_file("-"*80)
 
+
+
+print_to_screen_and_file("People interactions:")
+print_to_screen_and_file("-"*80)
+
+fp.close()
