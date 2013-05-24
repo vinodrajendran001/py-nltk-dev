abf89c2ccdfa14644b5394cca5d155eda9531302
ivonyte@gmail.com <ivonyte@gmail.com@f85f9711-d061-18d5-81b2-01f3bb4b83c6>
2013-05-24 18:36:03 +0000

    First summary is always made from one third of all article sentences length!

--- trunk/main.py
+++ trunk/main.py
@@ -1,6 +1,7 @@
-import nltk, data, summarize, pickle
-import ner, config, training, sys, utils, action, references
-from nltk.tree import Tree
+import nltk, data, pickle
+import ner, config, training, sys, utils, action, references, ph_reduction
+
+from summarize import SimpleSummarizer
 
 def print_to_screen_and_file(text):
 	print text
@@ -11,11 +12,15 @@ def print_to_screen_and_file(text):
 		fp.write(text+"\n")
 		fp.flush()
 
+def get_categories(article):
+	categories = []
+	#lets get all the categories from the article
+
 def run(path):
 	global fp 
 
-	if len(path) == 0:
-		path = "db/ivonyte-aiste/2011-7-8-1.txt" 
+	#if len(path) == 0:
+	#	path = "db/ivonyte-aiste/2011-7-8-1.txt" 
 	
 	# load article text
 	article = data.Article(path)
@@ -26,18 +31,22 @@ def run(path):
 	# show article text
 	print_to_screen_and_file("-"*80)
 	print_to_screen_and_file("Original article:")
+	print_to_screen_and_file("")
 	print_to_screen_and_file(article.text)
 	print_to_screen_and_file("-"*80)
 
 	# make the summary & show in console
 	print_to_screen_and_file("I Summary:")
-	instance = summarize.SimpleSummarizer()
-	print_to_screen_and_file(instance.summarize(article.text, config.MAX_SENTENCES))
+	print_to_screen_and_file("")
+	instance = SimpleSummarizer()
+	print_to_screen_and_file(instance.summarize(article.text, len(utils.sentences) / 3))
 	print_to_screen_and_file("-"*80)
 
 	#TODO: summary from phrase reduction
 	print_to_screen_and_file("II Summary:")
-	print_to_screen_and_file("Will be done soon :) ")
+	print_to_screen_and_file("")
+	#print_to_screen_and_file(test_regex_summary.get_summary(path))
+	print "".join(ph_reduction.PhraseReductor().find(utils.tagged_sentences))
 	print_to_screen_and_file("-"*80)
 	
 	# classification
@@ -46,16 +55,18 @@ def run(path):
 	feats = utils.bag_of_words(words, article.text, stemmer)
 	
 	classifier = pickle.load(file(config.BAYES_CLASSIFIER_FILE))
-	answer = classifier.classify(feats)
-	print_to_screen_and_file("BayesClassifier class: "+answer)
+	print_to_screen_and_file("BayesClassifier class: " + classifier.classify(feats))
+	
 	classifier = pickle.load(file(config.MAXENT_CLASSIFIER_FILE))
-	answer = classifier.classify(feats)
-	print_to_screen_and_file("MaxEntClassifier class: "+answer)
+	print_to_screen_and_file("MaxEntClassifier class: " + classifier.classify(feats))
+	
 	classifier = pickle.load(file(config.DTREE_CLASSIFIER_FILE))
-	answer = classifier.classify(feats)
-	print_to_screen_and_file("DecisionTreeClassifier class: "+answer)
+	print_to_screen_and_file("DecisionTreeClassifier class: " + classifier.classify(feats))
 	print_to_screen_and_file("-"*80)
 	
+	print_to_screen_and_file("Actual categories of the article:")
+	print "To come..."
+
 	# people actions
 	print_to_screen_and_file("People and their actions:")
 	work = action.Actions().find(utils.tagged_words, utils.tagged_sentences, utils.people)
@@ -82,5 +93,5 @@ if __name__ == "__main__":
 	#path = sys.argv[1]
 	#print path
 
-	path = "db/ivonyte-aiste/2011-7-8-1.txt" 
+	path = "db/kubilius-algirdas/2010-02-05-8.txt" 
 	run(path)
