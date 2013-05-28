ea6c3d228ef6a5121c5de90c665f966e5cbbe559
be.lietaus <be.lietaus@f85f9711-d061-18d5-81b2-01f3bb4b83c6>
2013-05-28 22:44:08 +0000

    Removed confusing/unsupported parameters, small tweaks.

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ fp = None
 
 def print_to_screen_and_file(text):
 	print text
-	#if type(text) == list:
-	#	for index, item in enumerate(text):
-	#		print index, item
-	#else:
 	fp.write(str(text)+"\n")
 	fp.flush()
 
@@ -25,31 +21,27 @@ def run(path):
 
 	# show article text
 	print_to_screen_and_file("-"*80)
-	print_to_screen_and_file("Original article:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("Original article:\n")
 	print_to_screen_and_file(article.text)
 	print_to_screen_and_file("-"*80)
-	print_to_screen_and_file("Categories:")
+	print_to_screen_and_file("Categories:\n")
 	print_to_screen_and_file(article.cats)
 	print_to_screen_and_file("-"*80)
 
 	# make the summary & show in console
-	print_to_screen_and_file("I Summary:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("I Summary:\n")
 	
 	instance = SimpleSummarizer()
 	# shorten the original article by one third
 	print_to_screen_and_file(instance.summarize(article.text, len(utils.sentences) / 3))
 	print_to_screen_and_file("-"*80)
 
-	#TODO: summary from phrase reduction to file!
-	print_to_screen_and_file("II Summary:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("II Summary:\n")
 	print_to_screen_and_file(" ".join(ph_reduction.PhraseReductor().find(utils.tagged_sentences)))
 	print_to_screen_and_file("-"*80)
 	
 	# classification
-	print_to_screen_and_file("Multiclass classification:")
+	print_to_screen_and_file("Multiclass classification:\n")
 	stemmer = nltk.stem.WordNetLemmatizer()
 	words = nltk.tokenize.wordpunct_tokenize(article.text)
 	feats = utils.bag_of_words(words, article.text, stemmer)
@@ -64,7 +56,7 @@ def run(path):
 	print_to_screen_and_file("DecisionTreeClassifier class: " + classifier.classify(feats))
 	print_to_screen_and_file("-"*80)
 	
-	print_to_screen_and_file("Binary classification:")
+	print_to_screen_and_file("Binary classification:\n")
 	title = ["BayesClassifier: ", "MaxEntClassifier: ", "DecisionTreeClassifier: "]
 	classifiers = [config.BAYES_CLASSIFIER_FILE_PATTERN, config.MAXENT_CLASSIFIER_FILE_PATTERN, config.DTREE_CLASSIFIER_FILE_PATTERN]
 	tags = ["A", "B", "C", "D", "E", "OTHER"]
@@ -78,8 +70,7 @@ def run(path):
 	print_to_screen_and_file("-"*80)
 
 	# people actions
-	print_to_screen_and_file("People and their actions:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("People and their actions:\n")
 	work = action.Actions().find(utils.tagged_words, utils.tagged_sentences, utils.people)
 	# print the updated info with people actions
 	for i, (key, value) in enumerate(work.items()):
@@ -87,16 +78,14 @@ def run(path):
 	print_to_screen_and_file("-"*80)
 
 	# anaphora
-	print_to_screen_and_file("Anaphoras:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("Anaphoras:\n")
 	refs = references.References().find(utils.people, utils.sentences, utils.tagged_sentences)
 	for ref, fullname, index in refs:
-		print_to_screen_and_file("Sentence["+str(index+1)+"]: " + ref + " - "+ fullname + "\n")
+		print_to_screen_and_file("Sentence["+str(index+1)+"]: " + ref + " - "+ fullname)
 	print_to_screen_and_file("-"*80)
 
 	# interactions
-	print_to_screen_and_file("People interactions:")
-	print_to_screen_and_file("")
+	print_to_screen_and_file("People interactions:\n")
 	inter = interactions.Interactor().find(refs, utils.tagged_sentences)
 	for index, item in enumerate(inter):
 		who, prp, what = item['who'], item['prp'], item['what']
@@ -118,6 +107,4 @@ if __name__ == "__main__":
 	if len(sys.argv) > 2 and sys.argv[1] == "-f":
 		run(sys.argv[2])
 	else:
-		# default
-		print "Loading previous article from pickles :)"
-		run("db/klementavicius-rimvydas/2011-12-03-1.txt")
+		print "No args specified, usage:\n\tpython main.py -f db/klementavicius-rimvydas/2011-12-03-1.txt"
